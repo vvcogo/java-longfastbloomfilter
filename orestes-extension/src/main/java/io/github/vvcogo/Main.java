f2b129961edeb8b3e7889dca027839f4f742b0ea
itsTaylz <itstaylz.main@gmail.com>
2023-08-06 19:56:57 +0100

    added guava & orestes dependencies

--- orestes-extension/src/main/java/io/github/vvcogo/Main.java
+++ orestes-extension/src/main/java/io/github/vvcogo/Main.java
@@ -1,8 +1,17 @@
 package io.github.vvcogo;
 
+import orestes.bloomfilter.BloomFilter;
+import orestes.bloomfilter.FilterBuilder;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BloomFilter<String> bf = new FilterBuilder(1_000_000, 0.1)
+                .buildBloomFilter();
+        bf.add("nub");
+        bf.add("hello");
+        System.out.println(bf.contains("nub"));
+        System.out.println(bf.contains("hello"));
+        System.out.println(bf.contains("aojrfweoijtgowgh"));
     }
 }
