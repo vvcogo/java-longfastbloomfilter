06c1cbb1eb30224f20f7e1df2979a4bfc72bf581
itsTaylz <itstaylz.main@gmail.com>
2023-08-09 20:33:40 +0100

    made guava adapter

--- guava-extension/src/main/java/io/github/vvcogo/GuavaBloomFilterAdapter.java
+++ guava-extension/src/main/java/io/github/vvcogo/GuavaBloomFilterAdapter.java
@@ -1,34 +1,51 @@
 package io.github.vvcogo;
 
+import com.google.common.hash.Funnel;
+
 public class GuavaBloomFilterAdapter<T> implements BloomFilter<T> {
 
-    public GuavaBloomFilterAdapter(BloomFilterConfiguration<? super T> config){
+    private final BloomFilterConfiguration<? super T> config;
+    private final com.google.common.hash.BloomFilter<T> emptyBloomFilter;
+    private com.google.common.hash.BloomFilter<T> bloomFilter;
 
+    public GuavaBloomFilterAdapter(BloomFilterConfiguration<? super T> config) {
+        this.config = config;
+        this.emptyBloomFilter = createBloomFilter();
+        this.bloomFilter = createBloomFilter();
     }
 
 
     @Override
     public void add(T element) {
-
+        this.bloomFilter.put(element);
     }
 
     @Override
     public boolean mightContains(T element) {
-        return false;
+        return this.bloomFilter.mightContain(element);
     }
 
     @Override
     public void clear() {
-
+        this.bloomFilter = createBloomFilter();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.bloomFilter.equals(this.emptyBloomFilter);
     }
 
     @Override
     public BloomFilterConfiguration<? super T> getConfiguration() {
-        return null;
+        return this.config;
+    }
+
+    private com.google.common.hash.BloomFilter<T> createBloomFilter() {
+        long expected = config.expectedNumberOfElements();
+        double fpp = config.falsePositiveRate();
+        Funnel<? super T> funnel = (from, into) -> {
+            into.putBytes(this.config.serializer().serialize(from));
+        };
+        return com.google.common.hash.BloomFilter.create(funnel, expected, fpp);
     }
 }
