77acbfab4e39f340e49e578e9fbe928e26303401
itsTaylz <itstaylz.main@gmail.com>
2023-08-13 21:36:54 +0100

    added more information in the test application

---
+++ bloomfilter-test/src/main/java/io/github/vvcogo/TestApplication.java
@@ -0,0 +1,106 @@
+package io.github.vvcogo;
+
+import io.github.vvcogo.longfastbloomfilter.framework.BloomFilterConfiguration;
+import io.github.vvcogo.longfastbloomfilter.framework.bloomfilter.BloomFilter;
+import io.github.vvcogo.longfastbloomfilter.framework.bloomfilter.StandardLongBloomFilter;
+import io.github.vvcogo.longfastbloomfilter.framework.hashing.HashAlgorithms;
+import io.github.vvcogo.longfastbloomfilter.framework.hashing.HashingAlgorithm;
+import io.github.vvcogo.longfastbloomfilter.framework.serialization.Serializer;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TestApplication {
+
+    private static final long BIT_SET = Integer.MAX_VALUE;
+    private static final long EXP = 10_000;
+    private static final int HASH_NUM = 5;
+    private static final double POS_RATE = 0.01;
+    private static final HashingAlgorithm HASH_FUNC = HashAlgorithms.MURMUR;
+    private static Serializer<String> sr = (elem) -> elem.getBytes();
+
+    private static BloomFilterConfiguration<String> bc = new BloomFilterConfiguration<>(BIT_SET, EXP, HASH_NUM, POS_RATE, HASH_FUNC, sr);
+    private static BloomFilter<String> bf = new StandardLongBloomFilter<>(bc);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        if(args.length < 2 || args.length > 3) {
+            String filePath = TestApplication.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+            String jarFileName = filePath.substring(filePath.lastIndexOf("/"), filePath.length());
+            System.err.println(String.format("Usage: .%s <insert file> <query file> [number of threads]", jarFileName));
+            System.exit(1);
+        }
+
+        String fileInsertPath = args[0];
+        String fileQueryPath = args[1];
+        List<String> listInsert = manageFile(fileInsertPath, "insert");
+        List<String> listQuery = manageFile(fileQueryPath, "query");
+
+        int numberOfThreads = Runtime.getRuntime().availableProcessors() - 1;
+        if (args.length == 3) {
+            int readNumber;
+            try {
+                readNumber = Integer.parseInt(args[2]);
+                if (readNumber <= 0) {
+                    System.err.println("Number of threads cannot be smaller than 1! Using: ");
+                } else {
+                    numberOfThreads = readNumber;
+                }
+            } catch (NumberFormatException e) {
+                System.err.println("Could not read number of threads!");
+            }
+        }
+
+
+        //threads---------------
+        System.out.println("\nCreating Thread Pool with " + numberOfThreads + " threads.\n");
+        ExecutorService exec = Executors.newFixedThreadPool(numberOfThreads);
+
+        List<Callable<Object>> callInsert = new ArrayList<>();
+        for(String elem : listInsert){
+            callInsert.add(Executors.callable(() -> {
+                bf.add(elem);
+                System.out.println(String.format("[Thread: %s] INSERTED: %s", Thread.currentThread().getName(), elem));
+            } ));
+        }
+        long start = System.currentTimeMillis();
+        exec.invokeAll(callInsert);
+        long elapsed = System.currentTimeMillis() - start;
+        System.out.println(String.format("\nFinished inserting %o elements in %o ms\n", callInsert.size(), elapsed));
+
+        AtomicInteger failCount = new AtomicInteger();
+        List<Callable<Object>> callQuery = new ArrayList<>();
+        for(String elem : listQuery){
+            callQuery.add(Executors.callable(() -> {
+                boolean result = bf.mightContains(elem);
+                System.out.println(String.format("[Thread: %s] QUERY (%s): %s", Thread.currentThread().getName(), elem, result));
+                if (!result)
+                    failCount.getAndIncrement();
+            }));
+        }
+
+        System.out.println();
+
+        start = System.currentTimeMillis();
+        exec.invokeAll(callQuery);
+        elapsed = System.currentTimeMillis() - start;
+        System.out.println(String.format("\nFinished querying %o elements in %o ms", callQuery.size(), elapsed));
+        System.out.println(String.format("%o/%o were false.\n", failCount.get(), callQuery.size()));
+        exec.shutdown();
+    }
+
+    public static List<String> manageFile(String path, String type){
+        List<String> result = null;
+        try {
+            result = FileReader.readFile(path);
+        } catch (FileNotFoundException e) {
+            System.err.println("Can not read " + type + " file.");
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return result;
+    }
+}
