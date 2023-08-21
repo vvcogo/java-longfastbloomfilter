ae452092f3dfd6a6e00d4db55b9498efb489d9b8
itsTaylz <itstaylz.main@gmail.com>
2023-08-22 00:45:12 +0100

    TestApplication refactor

--- bloomfilter-test/src/main/java/io/github/vvcogo/TestApplication.java
+++ bloomfilter-test/src/main/java/io/github/vvcogo/TestApplication.java
@@ -16,78 +16,47 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class TestApplication {
+public final class TestApplication {
 
     private static final Logger ROOT_LOGGER = LoggerFactory.getLogger("");
     private static final Logger PATTERNLESS_LOGGER = LoggerFactory.getLogger("patternless");
     private static final File EXTENSIONS_DIRECTORY = new File("extensions/");
 
     public static void main(String[] args) throws InterruptedException {
+        checkArguments(args);
+        
+        // for use with jConsole
+        Thread.sleep(3000);
 
-        if(args.length < 2 || args.length > 4) {
-            String filePath = TestApplication.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-            String jarFileName = filePath.substring(filePath.lastIndexOf("/"));
-            ROOT_LOGGER.error(String.format("Usage: .%s <insert file> <query file> <config file> <number of threads>", jarFileName));
-            System.exit(1);
-        }
-        Thread.sleep(2000);
         JavaExtensionLoader extensionLoader = new JavaExtensionLoader();
         loadExtensions(extensionLoader);
 
-        Set<String> loaded = extensionLoader.getLoadedExtensions();
-        if (!loaded.isEmpty()) {
-            ROOT_LOGGER.info("LOADED EXTENSIONS:");
-            for (String extension : loaded) {
-                ExtensionProperties properties = extensionLoader.getLoadedExtensionsData().get(extension).properties();
-                ROOT_LOGGER.info(String.format("\t> %s v%s", properties.getName(), properties.getVersion()));
-            }
-        }
-
-        String fileInsertPath = args[0];
-        String fileQueryPath = args[1];
-        Set<String> listInsert = manageFile(fileInsertPath, "insert");
-        Set<String> listQuery = manageFile(fileQueryPath, "query");
-
-        BloomFilterConfiguration<String> bc = null;
-        if (args.length >= 3) {
-            String configFilePath = args[2];
-            try (FileInputStream inputStream = new FileInputStream(configFilePath)) {
-                Properties props = new Properties();
-                props.load(inputStream);
-                bc = new BloomFilterConfigurationLoader<String>(props).getConfiguration();
-            } catch (FileNotFoundException e) {
-                ROOT_LOGGER.error("Config file not found!");
-                System.exit(1);
-            } catch (IOException e) {
-                ROOT_LOGGER.error("Failed to read config file!");
-                System.exit(1);
-            } catch (InvalidConfigurationException e) {
-                ROOT_LOGGER.error("Configuration was invalid! " + e.getMessage());
-                System.exit(1);
-            }
-        }
-
+        BloomFilterConfiguration<String> bc = loadConfiguration(args[2]);
         BloomFilter<String> bf = BloomFilterCreator.createBloomFilter(bc);
 
-        int numberOfThreads = Runtime.getRuntime().availableProcessors() - 1;
-        if (args.length == 4) {
-            int readNumber;
-            try {
-                readNumber = Integer.parseInt(args[3]);
-                if (readNumber <= 0) {
-                    ROOT_LOGGER.error("Number of threads cannot be smaller than 1! Using: ");
-                } else {
-                    numberOfThreads = readNumber;
-                }
-            } catch (NumberFormatException e) {
-                ROOT_LOGGER.error("Could not read number of threads!");
-            }
+        ExecutorService exec = createThreadPool(args.length == 4 ? args[3] : null);
+
+        List<String> listInsert = manageFile(args[0], "insert");
+        List<String> listQuery = manageFile(args[1], "query");
+        Set<String> setInsert = new HashSet<>(listInsert);
+
+        executeInserts(exec, bf, listInsert);
+        executeQueries(exec, bf, listQuery);
+        checkFalsePositives(bf, setInsert, listQuery);
+
+        exec.shutdown();
+    }
+
+    private static void checkArguments(String[] args) {
+        if(args.length < 3 || args.length > 4) {
+            String filePath = TestApplication.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+            String jarFileName = filePath.substring(filePath.lastIndexOf("/"));
+            ROOT_LOGGER.error(String.format("Usage: .%s <insert file> <query file> <config file> [number of threads]", jarFileName));
+            System.exit(1);
         }
+    }
 
-        //threads---------------
-        ROOT_LOGGER.info("Creating Thread Pool with " + numberOfThreads + " threads.\n");
-        ExecutorService exec = Executors.newFixedThreadPool(numberOfThreads);
-
+    private static void executeInserts(ExecutorService exec, BloomFilter<String> bf, List<String> listInsert) throws InterruptedException {
         List<Callable<Object>> callInsert = new ArrayList<>();
         for(String elem : listInsert){
             callInsert.add(Executors.callable(() -> {
@@ -100,7 +69,9 @@ public class TestApplication {
         long elapsed = System.currentTimeMillis() - start;
         PATTERNLESS_LOGGER.info("");
         ROOT_LOGGER.info(String.format("Finished inserting %s elements in %s ms%n", callInsert.size(), elapsed));
+    }
 
+    private static void executeQueries(ExecutorService exec, BloomFilter<String> bf, List<String> listQuery) throws InterruptedException {
         AtomicInteger failCount = new AtomicInteger();
         List<Callable<Object>> callQuery = new ArrayList<>();
         for (String elem : listQuery){
@@ -111,18 +82,18 @@ public class TestApplication {
                     failCount.getAndIncrement();
             }));
         }
-
-        start = System.currentTimeMillis();
+        long start = System.currentTimeMillis();
         exec.invokeAll(callQuery);
-        elapsed = System.currentTimeMillis() - start;
-
+        long elapsed = System.currentTimeMillis() - start;
         PATTERNLESS_LOGGER.info("");
         ROOT_LOGGER.info(String.format("Finished querying %s elements in %s ms", callQuery.size(), elapsed));
         ROOT_LOGGER.info(String.format("%s/%s were false.", failCount.get(), callQuery.size()));
+    }
 
+    private static void checkFalsePositives(BloomFilter<String> bf, Set<String> setInsert, List<String> listQuery) {
         List<String> falsePositives = new ArrayList<>();
         for (String query : listQuery) {
-            if (bf.mightContains(query) && !listInsert.contains(query)) {
+            if (bf.mightContains(query) && !setInsert.contains(query)) {
                 falsePositives.add(query);
             }
         }
@@ -131,11 +102,47 @@ public class TestApplication {
         } else {
             ROOT_LOGGER.info(String.format("False positives (%s): %s%n", falsePositives.size(), falsePositives));
         }
-        exec.shutdown();
     }
 
-    public static Set<String> manageFile(String path, String type){
-        Set<String> result = null;
+    private static BloomFilterConfiguration<String> loadConfiguration(String configFilePath) {
+        try (FileInputStream inputStream = new FileInputStream(configFilePath)) {
+            Properties props = new Properties();
+            props.load(inputStream);
+            return new BloomFilterConfigurationLoader<String>(props).getConfiguration();
+        } catch (FileNotFoundException e) {
+            ROOT_LOGGER.error("Config file not found!");
+            System.exit(1);
+        } catch (IOException e) {
+            ROOT_LOGGER.error("Failed to read config file!");
+            System.exit(1);
+        } catch (InvalidConfigurationException e) {
+            ROOT_LOGGER.error("Configuration was invalid! " + e.getMessage());
+            System.exit(1);
+        }
+        return null;
+    }
+
+    private static ExecutorService createThreadPool(String numbThreadsString) {
+        int numberOfThreads = Runtime.getRuntime().availableProcessors() - 1;
+        if (numbThreadsString != null) {
+            int readNumber;
+            try {
+                readNumber = Integer.parseInt(numbThreadsString);
+                if (readNumber <= 0) {
+                    ROOT_LOGGER.error("Number of threads cannot be smaller than 1! Using: ");
+                } else {
+                    numberOfThreads = readNumber;
+                }
+            } catch (NumberFormatException e) {
+                ROOT_LOGGER.error("Could not read number of threads!");
+            }
+        }
+        ROOT_LOGGER.info("Creating Thread Pool with " + numberOfThreads + " threads.\n");
+        return Executors.newFixedThreadPool(numberOfThreads);
+    }
+
+    private static List<String> manageFile(String path, String type){
+        List<String> result = null;
         try {
             result = FileReader.readFile(path);
         } catch (FileNotFoundException e) {
