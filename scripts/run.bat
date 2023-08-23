c3e7b1f340d000e0639c090e67ae938887c5d14b
kuukitenshi <lauracunha803@outlook.pt>
2023-08-23 20:26:33 +0100

    removed pause from script

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,2 +1,1 @@
 java -jar out/artifacts/bloomfilter_test_jar/bloomfilter-test.jar input.txt query.txt config.properties
-pause
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 java -jar out/artifacts/bloomfilter_test_jar/bloomfilter-test.jar input.txt query.txt config.properties
-pause
