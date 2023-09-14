16f2fa6419658b67db0d6625215a3c7648eceef2
kuukitenshi <lauracunha803@outlook.pt>
2023-09-14 20:59:12 +0100

    merge fix

--- bloomfilter-framework/src/main/java/io/github/vvcogo/longfastbloomfilter/framework/bloomfilter/StandardLongBloomFilter.java
+++ bloomfilter-framework/src/main/java/io/github/vvcogo/longfastbloomfilter/framework/bloomfilter/StandardLongBloomFilter.java
@@ -2,6 +2,7 @@ package io.github.vvcogo.longfastbloomfilter.framework.bloomfilter;
 
 import io.github.vvcogo.longfastbloomfilter.framework.bitset.AtomicLongBitSet;
 import io.github.vvcogo.longfastbloomfilter.framework.bitset.BitSet;
+import io.github.vvcogo.longfastbloomfilter.framework.bitset.LongBitSet;
 import io.github.vvcogo.longfastbloomfilter.framework.configuration.BloomFilterConfiguration;
 
 import java.util.Objects;
@@ -13,7 +14,7 @@ public class StandardLongBloomFilter<T> implements BloomFilter<T> {
 
     public StandardLongBloomFilter(BloomFilterConfiguration<? super T> config) {
         this.configuration = config;
-        this.bitSet = new AtomicLongBitSet(config.bitSetSize());
+        this.bitSet = new LongBitSet(config.bitSetSize()); //new AtomicLongBitSet(config.bitSetSize());
     }
 
     @Override
