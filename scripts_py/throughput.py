f7541a235792df5a0ae32e11c9a30c8ef9ab8977
itsTaylz <itstaylz.main@gmail.com>
2024-02-15 16:34:10 +0000

    organized python scripts

--- scripts_py/throughput.py
+++ scripts_py/throughput.py
@@ -10,9 +10,12 @@ if argc < 2:
 
 p = float(sys.argv[1])
 
-bloomfilter_types = ['longfastbloomfilter', 'guava']
+bloomfilter_types = ['longfastbloomfilter', 'guava', 'orestes']
 threads = [1, 2, 6, 12, 24, 48, 96]
 base_dir = 'logs'
+
+def format_number(num):
+    return format(round(num, 5), ',').replace(',', ' ')
 
 def read_logs_in_folder(path):
     logs_data = {'insert_throughput': 0, 'insert_latency': 0, 'query_latency': 0, 'query_throughtput': 0}
@@ -20,25 +23,42 @@ def read_logs_in_folder(path):
     insert_latency = []
     query_throughtput = []
     query_latency = []
+    
     for file_name in os.listdir(path):
         with open(os.path.join(path, file_name), 'r') as file:
             lines = file.readlines()
-            for i in range(len(lines)):
-                if 'Throughput:' in lines[i] or 'Latency:' in lines[i]:
-                    value = float(lines[i].split(' ')[-2])
-                    if 'inserting' in lines[i-1]:
-                        insert_throughput.append(value)
-                    elif 'inserting' in lines[i-2]:
-                        insert_latency.append(value)
-                    elif 'querying' in lines[i-1]:
-                        query_throughtput.append(value)
-                    elif 'querying' in lines[i-2]:
-                        query_latency.append(value)
-    logs_data['insert_throughput'] = numpy.mean(insert_throughput)
-    logs_data['insert_latency'] = numpy.mean(insert_latency)
-    logs_data['query_throughtput'] = numpy.mean(query_throughtput)
-    logs_data['query_latency'] = numpy.mean(query_latency)
+            in_th, in_lat, quer_th, quer_lat = data_avg_per_iteration(lines)
+            insert_throughput.append(in_th)
+            insert_latency.append(in_lat)
+            query_throughtput.append(quer_th)
+            query_latency.append(quer_lat)
+            
+    logs_data['insert_throughput'] = format_number(numpy.mean(insert_throughput))
+    logs_data['insert_latency'] = format_number(numpy.mean(insert_latency))
+    logs_data['query_throughtput'] = format_number(numpy.mean(query_throughtput))
+    logs_data['query_latency'] = format_number(numpy.mean(query_latency))
     return logs_data
+
+
+def data_avg_per_iteration(lines):
+    insert_throughput = []
+    insert_latency = []
+    query_throughtput = []
+    query_latency = []
+    for i in range(len(lines)):
+        if 'Throughput:' in lines[i] or 'Latency:' in lines[i]:
+            value = float(lines[i].split(' ')[-2])
+        if 'inserting' in lines[i-1]:
+            insert_throughput.append(value)
+        elif 'inserting' in lines[i-2]:
+            insert_latency.append(value)
+        elif 'querying' in lines[i-1]:
+            query_throughtput.append(value)
+        elif 'querying' in lines[i-2]:
+            query_latency.append(value)
+    return numpy.mean(insert_throughput), numpy.mean(insert_latency), numpy.mean(query_throughtput), numpy.mean(query_latency)
+
+
 
 logs_data = {}
 
